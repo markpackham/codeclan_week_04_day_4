92a0f9b94fe45bfe38076b421b906c54883ca1d1
mark <markpackham1@gmail.com>
2020-04-23 11:07:07 +0100

    Delete Student

--- students_controller.rb
+++ students_controller.rb
@@ -29,7 +29,22 @@ post "/students" do
 end
 
 # edit
+get "/students/:id/edit" do
+  id = params[:id].to_i
+  @student = Student.find(id)
+  erb(:edit)
+end
 
 # update
 
+post "/students/:id" do
+  Student.new(params).update
+  redirect to '/students'
+end
+
 # destroy
+post '/students/:id/delete' do # delete
+    order = Student.find( params[:id] )
+    order.delete()
+    redirect to '/students'
+end
